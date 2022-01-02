1d6adcca0baf865b954bd981a89e50c27e2fb606
Branden Palmer <brandenpalmer08@gmail.com>
2022-01-02 15:14:24 +0700

    add function to toggle hide/unhide

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const makeDropDownMenuFromMainItemAndSubItems = (mainItem, subItems) => {
+  if (!mainItem || !subItems)
+    return console.error(
+      "Parameters required for ",
+      makeDropDownMenuFromMainItemAndSubItems
+    );
   hideAllSubItems();
-  mainItem.addEventListener("click", unHideAllSubItems);
-  mainItem.addEventListener("mouseenter", unHideAllSubItems);
-  mainItem.addEventListener("mouseleave", hideAllSubItems);
+  mainItem.addEventListener("click", toggleHideSubItems);
+
+  function toggleHideSubItems() {
+    areSubItemsHidden() ? unHideAllSubItems() : hideAllSubItems();
+  }
 
   function hideAllSubItems() {
     subItems.forEach((item) => (item.style.display = "none"));
   }
 
   function unHideAllSubItems() {
-    subItems.forEach((item) => (item.style.display = "list-item"));
+    subItems.forEach((item) => (item.style.display = ""));
+  }
+
+  function areSubItemsHidden() {
+    return subItems.every((item) => item.style.display === "none");
   }
 };
-
-//tests
-const menu1 = document.getElementById("menu1");
-const subItemsMenu1 = [...menu1.querySelectorAll("li")];
-const menu2 = document.getElementById("menu2");
-const subItemsMenu2 = [...menu2.querySelectorAll("li")];
-const menu3 = document.getElementById("menu3");
-const subItemsMenu3 = [...menu3.querySelectorAll("li")];
-makeDropDownMenuFromMainItemAndSubItems(menu1, subItemsMenu1);
-makeDropDownMenuFromMainItemAndSubItems(menu2, subItemsMenu2);
-makeDropDownMenuFromMainItemAndSubItems(menu3, subItemsMenu3);
